6b5d71adede5e19c5dbddc495c055fa26443bc05
Andreas Strauman <andreas@strauman.net>
2015-05-11 20:40:59 +0200

    hide build_status file

--- bacon_build_status.py
+++ bacon_build_status.py
@@ -2,6 +2,23 @@ import sublime, sublime_plugin, os
 
 callback_active=False
 
+file_hidden=False
+
+def hide_build_status_file():
+	# if file_hidden:
+	# 	return
+	file_hidden=True
+	prefs = sublime.load_settings('Preferences.sublime-settings')
+	hidden_files=prefs.get("file_exclude_patterns")
+	if "build_status" in hidden_files:
+		return
+	
+	hidden_files.append("build_status")
+	prefs.set("file_exclude_patterns", hidden_files)
+	sublime.save_settings('Preferences.sublime-settings')
+
+hide_build_status_file()
+
 def set_status_all_views(status):
 	for view in sublime.active_window().views():
 		view.set_status("build_status", status)
@@ -25,3 +42,4 @@ def update_build_status():
 if not callback_active:
 	callback_active=True
 	sublime.set_timeout(update_build_status, 1000)
+
