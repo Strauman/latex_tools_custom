ad441f533118a9891ec3e9b577021c3ac7a4fa8b
Andreas Strauman <andreas@strauman.net>
2015-05-23 13:21:37 +0200

    List dupes

--- unimplemented.py
+++ unimplemented.py
@@ -10,6 +10,9 @@ class BaconUnimplementedBase(sublime_plugin.WindowCommand):
 		self.topfolder=""
 		self.mode=False
 
+	def beforeRun(self):
+		pass
+
 	def diff_lists(a, b):
 		b = set(b)
 		return [aa for aa in a if aa not in b]
@@ -17,7 +20,6 @@ class BaconUnimplementedBase(sublime_plugin.WindowCommand):
 	def is_enabled(self):
 		self.window=sublime.active_window()
 		for f in self.window.folders():
-			print(path.join(f, self.input_dirname))
 			if path.isdir(path.join(f, self.rootfiles_dirname)) and path.isdir(path.join(f, self.input_dirname)):
 				return True
 		return False
@@ -32,6 +34,7 @@ class BaconUnimplementedBase(sublime_plugin.WindowCommand):
 				self.root_files=self.get_tex_files(path.join(f, self.rootfiles_dirname))
 
 	def run(self):
+		self.beforeRun()
 		self.setvars()
 		self.show_list()
 
@@ -48,12 +51,12 @@ class BaconUnimplementedBase(sublime_plugin.WindowCommand):
 		self.missing=list(set(self.awaits_files)-set(self.input_files))
 
 		if (self.mode=="unimplemented"):
-			diff=self.unimplemented
+			diff=list(set(self.unimplemented))
 		elif self.mode=="missing":
-			diff=self.missing
+			diff=list(set(self.missing))
 		else:
 			return
-		self.window.show_quick_panel(diff, self.setChosenToClipboard)
+		self.window.show_quick_panel(sorted(set(diff)), self.setChosenToClipboard)
 
 	def setChosenToClipboard(self, index):
 		if index==-1:
@@ -74,22 +77,44 @@ class BaconUnimplementedBase(sublime_plugin.WindowCommand):
 					continue
 			files.append(self.rreplace(filename, '.tex', ''))
 		return files
+
+
+class BaconListDuplicatesCommand(BaconUnimplementedBase):	
+	def beforeRun(self):
+		self.mode=False
+
+	def show_list(self):
+		self.awaits_files=[]
+		self.dupes=[]
+		for rf in self.root_files:
+			self.dupeFile(rf)
+		
+		self.window.show_quick_panel(sorted(set(self.dupes)), self.setChosenToClipboard)
+	
+	def setChosenToClipboard(self, index):
+		if index==-1:
+			return
+		sublime.set_clipboard("\\awaits{"+self.dupes[index]+"}")
+	def dupeFile(self, file):
+		file_contents=open(path.join(self.topfolder, self.rootfiles_dirname, file)+".tex", 'r').read()
+		for awaits in re.findall(r"\\awaits{([^}]+)}", file_contents):
+			if(awaits in self.awaits_files):
+				self.dupes.append(awaits)
+			else:
+				self.awaits_files.append(awaits)
+
 		
 class BaconListUnimplementedCommand(BaconUnimplementedBase):	
-	def __init__(self, *args, **kwargs):
-		super(BaconListUnimplementedCommand, self).__init__(*args, **kwargs)
+	def beforeRun(self):
 		self.mode="unimplemented"
 
 class BaconListMissingCommand(BaconUnimplementedBase):	
-	def __init__(self, *args, **kwargs):
-		super(BaconListMissingCommand, self).__init__(*args, **kwargs)
-		self.mode="missing"
-class BaconInsertAllUnimplementedCommand(BaconUnimplementedBase):
-	def __init__(self, *args, **kwargs):
-		super(BaconInsertAllUnimplementedCommand, self).__init__(*args, **kwargs)
-		self.mode=False
+	def beforeRun(self):
+		self.mode="unimplemented"
 
+class BaconInsertAllUnimplementedCommand(BaconUnimplementedBase):
 	def run(self):
+		self.mode=False
 		self.setvars()
 		self.show_list()
 		view=self.window.active_view()
@@ -98,7 +123,6 @@ class BaconInsertAllUnimplementedCommand(BaconUnimplementedBase):
 			contents=u''
 			for f in self.unimplemented:
 				contents+=u'\\awaits{'+f+'}\n'
-			print(contents)
 			edit=view.begin_edit()
 			view.insert(edit, line.begin(), contents)
 			view.end_edit(edit)
